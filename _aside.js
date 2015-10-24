292bec5197d488e3c45219fa9b07750e2df441d4
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-24 10:27:54 -0500

    Lecture 15: Arrays can contain functions, and functions can be called by indexing into them

--- _aside.js
+++ _aside.js
@@ -1,26 +1,12 @@
-//Dependency Injection: giving a function an object. 
+var things = 
+    [1,
+     '2',
+     function() {
+         alert("Hello!");
+     }
+    ];
 
-//Rather than creating an object inside a function, you pass it to the function
+// arrays can hold anything including functions, and those functions can be called by indexing into that array and calling the function like so:
+things[2]();
 
-var Person = function(firstname, lastname) {
-    
-    this.firstname = firstname;
-    this.lastname = lastname;
-}
-
-//logPerson() is dependent upon the variable john. if something were to change about john, you would have to change it inside this function. this makes for complex code. instead, we'll rewrite it with Dependency Injection below
-function logPerson()
-{
-    var john = new Person('John', 'Doe');
-    console.log(john);
-}
-
-logPerson();
-
-// refactored with Dependency Injection. We are passing the object to the function instead of creating the object inside the function.
-function logPersonDI(person) {
-    console.log(person);
-}
-
-var john = new Person('John', 'Doe');
-logPersonDI(john);
+console.log(things);
