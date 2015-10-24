0d7a561b79a6426e9cff3ec830e034c4f1447242
BarrettNashville <AG17399@AG1719WM25212.net.ads.state.tn.us>
2015-10-24 09:27:01 -0500

    Lecture 11: A basic understanding of $scope

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 var myApp = angular.module('myApp', []); // takes the name of your app and an array of dependencies
 
-myApp.Controller('mainController', function() {
+myApp.controller('mainController', function($scope) {
+    
+    // we can add properties and functions to the $scope
+    $scope.name = 'Jane Doe';
+    $scope.occupation = 'Coder';
+    
+    $scope.getname = function() {
+        return 'John Doe';
+    }
+    
+    $scope.getname();
+    
+    console.log($scope);
     
 });
+
+// $scope is a way of doing dependency injection in angular
+
+// the $scope becomes the middle piece between the view and the controller. 
+
+// the $scope defines that data that goes back and forth between the function above and what appears in the HTML in the mainController section
