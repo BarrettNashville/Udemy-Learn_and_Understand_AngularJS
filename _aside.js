d8232d06cf036309721793609bad7db658c14a20
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-11-13 12:41:51 -0600

    Lecture 26: How the # hash values of the URL form basis of Single Page Apps

--- _aside.js
+++ _aside.js
@@ -1,15 +1,27 @@
-/* He shows an example Visual Studio ASP.NET app that has a database of rules and it exposes that info through a JSON API. He then retrieves those rules using the XMLHttpRequest() method built into JavaScript. Here is the important part of that code */
-
-var rulesrequest = new XMLHttpRequest();
-rulesrequest.onreadystatechange = function() {
+window.addEventListener('hashchange', function() {
     
-    /* need to wrap in $scope.$apply since we are outside the angular context in regular JavaScript code */
-    $scope.$apply(function() {
-        if(rulesrequest.readyState == 4 && rulesrequest.status = 200) {
-            $scope.rules = JSON.parse(rulesrequest.responseText);
-        }    
-    });
-}
-
-rulesrequest.open("GET", "http://localhost:54765/api", true);
-rulesrequest.send();
+    /* shows that this event fires when the hash (the part after the # in the url) has changed */
+    //console.log('Hash changed!: ' + window.location.hash);
+    
+    /* let's do different things based upon the value of the hash, which by the way might look like a folder structure */
+    if(window.location.hash === '#/bookmark/1') {
+        console.log('Page 1 is cool');
+    }
+    
+    if(window.location.hash === '#/bookmark/2') {
+        console.log('Let me go get Page 2.');
+    }
+    
+    if(window.location.hash === '#/bookmark/3') {
+        console.log('Here\'s page 3.');
+    }
+    
+    /* this can be tested by just changing the value in the url of the browser */
+    
+    /* from the browser's perspective, you're not telling it to go out and get a new page because the anchor doesn't exist.
+       But in the JavaScript I can look at the value of the hash and do whatever I want.
+       This concept is a fundamental key to single page applications.
+       You still go out and get new html behind the scenes using AJAX / JSON/ APIs without refreshing the page.
+       You pretend like each hash value corresponds to a page. 
+    */
+});
