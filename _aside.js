542d17a1a25f573d649ebd1f71260304c5289a38
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-11-13 11:49:56 -0600

    Lecture 23: Example of XMLHttpRequest() as a precursor to the HTTP object used by Angular

--- _aside.js
+++ _aside.js
@@ -1,5 +1,15 @@
-/* interpolation: create a string by combining strings and placeholders
+/* He shows an example Visual Studio ASP.NET app that has a database of rules and it exposes that info through a JSON API. He then retrieves those rules using the XMLHttpRequest() method built into JavaScript. Here is the important part of that code */
 
-'My name is' + name is interpolated and the result is 'My name is Barrett'
+var rulesrequest = new XMLHttpRequest();
+rulesrequest.onreadystatechange = function() {
+    
+    /* need to wrap in $scope.$apply since we are outside the angular context in regular JavaScript code */
+    $scope.$apply(function() {
+        if(rulesrequest.readyState == 4 && rulesrequest.status = 200) {
+            $scope.rules = JSON.parse(rulesrequest.responseText);
+        }    
+    });
+}
 
-*/
+rulesrequest.open("GET", "http://localhost:54765/api", true);
+rulesrequest.send();
