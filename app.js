d7ccf4591dcb587a4a053fc69208a02bc09f42ca
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-11-19 12:50:43 -0600

    Lecture 44: Retrieve API info and display on page

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ weatherApp.config(function($routeProvider) {
         controller: 'forecastController'
     })    
     
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'
+    })
 });
 
 // SERVICES
@@ -33,10 +37,38 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
         cityService.city = $scope.city;
     });
     
+
+
+    
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
     
     $scope.city = cityService.city;
     
+    $scope.days = $routeParams.days || 2;
+    
+    // API call configuration
+
+    $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily',   {
+        APPID: '69bba652920768e95dd9517369c36b9e',
+        callback: 'JSON_CALLBACK',
+        cnt: 5
+        }, { get: { method: 'JSONP' } }
+    );
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q:$scope.city, cnt: $scope.days})
+    
+    $scope.convertToFahrenheit = function(degK) {
+        
+        return Math.round((1.8 * (degK - 273)) + 32);
+        
+    }
+    
+    $scope.convertToDate = function(dt) {
+        
+        return new Date(dt * 1000);
+    };
+    
 }]);
+
