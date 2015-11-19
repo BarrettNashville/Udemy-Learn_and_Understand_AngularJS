5284312b863e18d7612ba034d04581b9b73fc8fe
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-11-19 12:09:53 -0600

    Lecture 42a: A basic template for a SPA Angular App

--- app.js
+++ app.js
@@ -1,80 +1,30 @@
-var myApp = angular.module('myApp', ['ngRoute']); 
+// MODULE
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']) ;
 
-myApp.config(function($routeProvider) {
-   
+// ROUTES
+weatherApp.config(function($routeProvider) {
+    
     $routeProvider
     
     .when('/', {
-        templateUrl: 'pages/main.html',
-        controller: 'mainController'
+        templateUrl: 'pages/home.html',
+        controller: 'homeController'
     })
-    
-    .when('/second', {
-        templateUrl: 'pages/second.html',
-        controller: 'secondController'
-    })
-    
-    .when('/second/:num', {
-        templateUrl: 'pages/second.html',
-        controller: 'secondController'
-    })
+    .when('/forecast', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'
+    })    
     
 });
 
 
-myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
-
-    $scope.people = [
-        {
-        name: 'John Doe',
-        address: '555 Main St.',
-        city: 'New York',
-        state: 'NY',
-        zip: '11111'
-        },
-        {
-        name: 'Jane Doe',
-        address: '333 Second St.',
-        city: 'Buffalo',
-        state: 'NY',
-        zip: '22222'
-        },
-        {
-        name: 'George Doe',
-        address: '111 Third St.',
-        city: 'Miami',
-        state: 'FL',
-        zip: '33333'
-        }
-        ]
-    
-    $scope.formattedAddress = function(person) {
-        
-        return person.address + ', ' + person.city + ', ' + person.state + ' ' + person.zip;
-        
-    };
-    
-}]);
-
-
-myApp.controller('secondController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {
-    
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', function($scope) {
     
     
 }]);
 
-myApp.directive("searchResult", function() {
-   return {
-       restrict: 'A, E, C, M', 
-       templateUrl: 'directives/searchresult.html' ,
-       replace: true ,
-       scope: {
-           personObject: "=" ,
-           formattedAddressFunction: "&"
-       },
-       transclude: true
-    }
-       
-});
-
-// transclusion: including one document in another. you place a copy of one document at a particular point inside another. 
+weatherApp.controller('forecastController', ['$scope', function($scope) {
+    
+    
+}]);
