45b04dc68da9fe723c76313ba9893e8da831e9a3
BarrettNashville <ag17399@AG1719WM25212.net.ads.state.tn.us>
2015-11-20 12:06:17 -0600

    Lecture 46: Complete First Draft of Single Page Weather App

---
+++ controllers.js
@@ -0,0 +1,40 @@
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function() {
+        cityService.city = $scope.city;
+    });
+       
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || '2';
+    
+    // API call configuration
+
+    $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily',   {
+        APPID: '69bba652920768e95dd9517369c36b9e',
+        callback: 'JSON_CALLBACK',
+        cnt: 5
+        }, { get: { method: 'JSONP' } }
+    );
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q:$scope.city, cnt: $scope.days})
+    
+    $scope.convertToFahrenheit = function(degK) {
+        
+        return Math.round((1.8 * (degK - 273)) + 32);
+        
+    }
+    
+    $scope.convertToDate = function(dt) {
+        
+        return new Date(dt * 1000);
+    };
+    
+}]);
